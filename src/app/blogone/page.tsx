ccd15f07c9b528fbe9670a9f8f4cdd43a18c1f11
Ashok <ashokkumaran777@gmail.com>
2024-05-31 18:42:48 +0400

    Modal Popup

--- src/app/blogone/page.tsx
+++ src/app/blogone/page.tsx
@@ -1,7 +1,24 @@
+import './blogone.css'
 export default function Blogone() {
   return (
-    <div>
-      Blogone
-    </div>
+    <>
+      <div className='test_project_nextjs'>
+
+        <div className="container mx-auto">
+          <div className="interior">
+            <a className="btn" href="#open-modal">Click Here &rarr;</a>
+          </div>
+        </div>
+
+        <div id="open-modal" className="modal-window">
+          <div>
+            <a href="#" title="Close" className="modal-close">Close</a>
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,35 @@
+"use client"
 import './main.css'
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import maindata from '../Data/maindata'
 export default function Home() {
+
   return (
+
     <>
-      <div className="test_project_main">
-        <div className="container mx-auto">
-          <div className='test_project_content'>
-            <div className='test_project_card'>
-              {
-                maindata.map((data) => {
-                  return (
-                    <div className='test_project_title_contents'>
-                      <div className='test_project_title_contents_text'>
-                        <h6>{data.title}</h6>
-                        <div className='test_project_title_button'>
-                          <Link href={data.route}><button className='test_project_button'>{data.button}</button></Link>
+          <div className="test_project_main">
+            <div className="container mx-auto">
+              <div className='test_project_content'>
+                <div className='test_project_card'>
+                  {
+                    maindata.map((data) => {
+                      return (
+                        <div className='test_project_title_contents'>
+                          <div className='test_project_title_contents_text'>
+                            <h6>{data.title}</h6>
+                            <div className='test_project_title_button'>
+                              <Link href={data.route}><button className='test_project_button'>{data.button}</button></Link>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                  )
-                })
-              }
+                      )
+                    })
+                  }
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
     </>
   );
 }
